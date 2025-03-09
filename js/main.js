028cc5ca63ee0f4a748ee8637b7ba9ae16d1bc69
risky <n11werthmann@gmail.com>
2025-03-09 19:59:11 +0100

    Update

---
+++ js/main.js
@@ -0,0 +1,4 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const contributionChartContainer = document.querySelector('.contribution-chart-container');
+    contributionChartContainer.scrollLeft = contributionChartContainer.scrollWidth;
+});
